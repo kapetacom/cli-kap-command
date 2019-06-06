f469a9d9004d0226a837fb89a7137ed99a8f9804
Henrik von Hofmeister <hh@cloudscrape.com>
2019-06-06 16:20:21 +0200

    BW-57: Initial commit for blockctl command module

---
+++ index.js
@@ -0,0 +1,36 @@
+const Command = require('commander').Command;
+const PARENT_NAME = process.env.BLOCKWARE_PARENT_CLI || 'blockctl';
+
+class BlockwareCommand {
+    constructor(name, version) {
+        this._program = new Command();
+        this._program.name(PARENT_NAME + ' ' + name).version(version);
+    }
+
+    parentName() {
+        return PARENT_NAME;
+    }
+
+    program() {
+        return this._program;
+    }
+
+    start() {
+
+        if (process.argv.length < 3) {
+            console.error('No command specified\n');
+            this._program.help();
+            process.exit(1);
+        }
+
+        this._program.on('command:*', () => {
+            console.error('Invalid command: %s\n', this._program.args.join(' '));
+            this._program.help();
+            process.exit(1);
+        });
+
+        this._program.parse(process.argv);
+    }
+}
+
+module.exports = BlockwareCommand;
