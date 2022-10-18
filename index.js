ecce9747a7709555a84d24f5a14431fd9d275daf
Henrik Hofmeister <henrik@newdawn.dk>
2022-10-18 12:18:32 +0200

    Upgraded commandor module

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const Command = require('commander').Command;
+const {Command} = require('commander');
 const PARENT_NAME = process.env.BLOCKWARE_PARENT_CLI || 'blockctl';
 
 class BlockwareCommand {
     constructor(name, version) {
         this._program = new Command();
-        this._program.name(PARENT_NAME + ' ' + name).version(version);
+        this._program
+            .name(PARENT_NAME + ' ' + name)
+            .version(version);
     }
 
     parentName() {
